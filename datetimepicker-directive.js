d1d0eb47c016219a953a9fc39a37f13a4616daad
Cres Jie <cresjie@gmail.com>
2017-06-11 17:53:39 +0800

    Update datetimepicker-directive.js

--- datetimepicker-directive.js
+++ datetimepicker-directive.js
@@ -4,14 +4,19 @@ angular.module('DateTimePicker',[])
       			require: '?ngModel',
       			restrict: 'AE',
       			link: function(scope, elem, attr, ngModel){
-      				
+
       				elem.datetimepicker(scope.$eval(attr.datetimepicker)).on('dp.change', function(){
       					ngModel.$setViewValue(this.value); 
       				});
       				
 	  		         
 	  		        ngModel.$render = function(){
-	  		        	elem.data("DateTimePicker").date(new moment(ngModel.$viewValue) ) ;
+	  		        	if(ngModel.$viewValue) {
+	  		        		elem.data("DateTimePicker").date(new moment(ngModel.$viewValue) ) ;
+	  		        	}else {
+	  		        		elem.data("DateTimePicker").clear();
+	  		        	}
+	  		        	
 	  		        }
       			}
 
